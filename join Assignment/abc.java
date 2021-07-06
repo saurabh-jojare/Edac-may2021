6d9798a532b40bb0560243e242e5d527b2c57e4a
saurabh-jojare <35952579+saurabh-jojare@users.noreply.github.com>
2021-07-06 23:51:26 +0530

    Update abc.java

--- join Assignment/abc.java
+++ join Assignment/abc.java
@@ -1,1 +1,23 @@
+public class Main
+{
+
+    public static void main(String[] args) 
+    {
+       // System.out.println(i);
+        System.out.println(++i);
+        System.out.println("in main");
+     
+    }
+
+    static 
+    {
+        System.out.println("in static block");
+        String[] args = new String[10];
+        main(args);
+        new Main().main(args);;
+    }
+    static int i =10 ;
+
+	
+}
 
